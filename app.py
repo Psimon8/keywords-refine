e640c2771c5981fc0dda3dab7c3274ac68d9d0ce
Psimon8 <121551167+Psimon8@users.noreply.github.com>
2024-04-26 17:44:29 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,11 @@ import numpy as np
 import pandas as pd
 from collections import defaultdict
 
+st.set_page_config(
+    layout="wide",
+    page_title="Keyword Refine",
+    page_icon="🍉"
+)
 
 def process_value(value):
     special_chars_map = {
