d660e0518206a4110f19d46762ff5c84418b6679
Psimon8 <121551167+Psimon8@users.noreply.github.com>
2024-04-28 10:22:24 +0200

    Update app.py

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ def levenshtein_distance(a, b):
         matrix[0][j] = j
 
     for i in range(1, len(b) + 1):
-        for j en range(1, len(a) + 1):
+        for j in range(1, len(a) + 1):
             if b[i - 1] == a[j - 1]:
                 matrix[i][j] = matrix[i - 1][j - 1]
             else:
